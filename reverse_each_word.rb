c38d74543303550415d8be637cd6946b2c86e07c
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-17 16:45:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,4 @@
 def reverse_each_word(string)
-  arr = []
-  arr << string
-  arr.each do |x|
-    i = 0 
-    while i < string.length do 
-      i.reverse
-    end 
-     i += 1 
-end 
-end 
-def reverse_each_word(string)
   words = string.split(" ")
   new_sentence = []
   words.collect do |word|
